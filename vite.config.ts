fbe165e2972e9fca54d15d8d707b9e436ebcec18
Jes015 <jes.18.sin.15@gmail.com>
2023-10-24 10:25:17 -0500

    feat: add crx.js (#1)
    close #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import manifest from './manifest.json'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    crx({ manifest }),
+  ],
 })
